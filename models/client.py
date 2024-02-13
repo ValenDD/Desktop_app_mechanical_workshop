0cadb6c0bffd79709fa75c2f5a2f99c804b4037b
ValentinDutra <108243077+ValenDD@users.noreply.github.com>
2024-02-13 11:58:06 -0300

    asd

--- models/client.py
+++ models/client.py
@@ -1,5 +1,9 @@
 class Client:
-    def __init__(self, name, phone, vehicles=None):
+    def __init__(self, name, phone):
         self._id_client = None
         self._name = name
         self._phone = phone
+        self.works = []
+
+    #def add_work(self, work):
+        #self.works.append(work)
